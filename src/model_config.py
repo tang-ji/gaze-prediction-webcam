805c562e8cc9ad1865baad9b6e059375639a744d
tangji08 <ji.tang@polytechnique.edu>
2020-11-21 12:24:36 +0100

    new divisions

--- src/model_config.py
+++ src/model_config.py
@@ -1,14 +1,15 @@
 img_size = 64
-n_channel = 3
+n_channel = 1
 mask_size = 25
 
-conv1_eye_size = 11
-conv1_eye_out = 32
+conv1_eye_size = 9
+conv1_eye_out = 64
+conv1_eye_strides = 2
 pool1_eye_size = 2
 pool1_eye_stride = 2
 
-conv2_eye_size = 7
-conv2_eye_out = 32
+conv2_eye_size = 5
+conv2_eye_out = 64
 pool2_eye_size = 2
 pool2_eye_stride = 2
 
@@ -17,18 +18,14 @@ conv3_eye_out = 64
 pool3_eye_size = 2
 pool3_eye_stride = 2
 
-conv4_eye_size = 1
-conv4_eye_out = 64
-pool4_eye_size = 2
-pool4_eye_stride = 2
-
-conv1_face_size = 11
-conv1_face_out = 32
+conv1_face_size = 9
+conv1_face_out = 64
+conv1_face_strides = 2
 pool1_face_size = 2
 pool1_face_stride = 2
 
-conv2_face_size = 7
-conv2_face_out = 32
+conv2_face_size = 5
+conv2_face_out = 64
 pool2_face_size = 2
 pool2_face_stride = 2
 
@@ -36,11 +33,6 @@ conv3_face_size = 3
 conv3_face_out = 64
 pool3_face_size = 2
 pool3_face_stride = 2
-
-conv4_face_size = 1
-conv4_face_out = 64
-pool4_face_size = 2
-pool4_face_stride = 2
 
 fc_eye_size = 128
 fc_face_size = 128
